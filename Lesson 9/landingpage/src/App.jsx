7333ea6dca05a826ebe00941c064ae0dbfbb642e
nadeemtadvi <nadeemtadvi1@gmail.com>
2025-01-02 20:19:14 +0530

    make nimation with canvas and locomotive and gsap

--- Lesson 9/landingpage/src/App.jsx
+++ Lesson 9/landingpage/src/App.jsx
@@ -1,15 +1,48 @@
-import React from 'react'
-import './App.css'
+import React, { useEffect } from "react";
+import "./App.css";
+import Canvas from "./Canvas";
+import data from "./data";
+import LocomotiveScroll from 'locomotive-scroll';
 
 
 const App = () => {
-  return (
-    <div className='bg-black text-white min-h-screen'>
-      <h1 className="text-3xl font-bold underline text-slate-600">
-      Hello world!
-    </h1>
-    </div>
-  )
-}
+  useEffect(() => {
+    const locomotiveScroll = new LocomotiveScroll();
+  }, []);
 
-export default App
+  return (
+    <>
+    <div className=" text-white min-h-screen w-full relative">
+      {data[0].map((canvasdets, index) => (
+        <Canvas details={canvasdets} />
+      ))}
+    </div>
+    {/* <div className=" text-white min-h-screen w-full relative">
+      {data[1].map((canvasdets, index) => (
+        <Canvas details={canvasdets} />
+      ))}  
+    </div>
+    <div className=" text-white min-h-screen w-full relative">
+      {data[2].map((canvasdets, index) => (
+        <Canvas details={canvasdets} />
+      ))}
+    </div> */}
+    <div className="text-white h-screen w-full bg-black">
+      <nav className="fixed top-0 w-full flex justify-between px-20 py-8">
+        <div className="brand">
+          <h3>BRAND.</h3>
+        </div>
+        <div className="links flex gap-10 text-white">
+          {["Home", "About", "Projects", "Contact"].map((link) => (
+            <a key={link} href={`#${link.toLowerCase()}`}>
+              {link}
+            </a>
+          ))}
+        </div>
+      </nav>
+      </div>
+    </>
+  );
+};
+
+export default App;
