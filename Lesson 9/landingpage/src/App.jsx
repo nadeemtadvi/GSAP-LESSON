56241297d28a5c1226a4e260223212a676043bb6
nadeemtadvi <nadeemtadvi1@gmail.com>
2025-01-02 17:08:08 +0530

    Add Lesson 9 React Gsap project

---
+++ Lesson 9/landingpage/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+
+const App = () => {
+  return (
+    <div className='bg-black text-white min-h-screen'>
+      <h1 className="text-3xl font-bold underline text-slate-600">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default App
